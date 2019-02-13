84362ff8a11fcab7fc890dc62468b2327fab5f97
adriennemiller <millera.92@gmail.com>
2019-02-12 17:08:45 -0800

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style_key, language_hash|
+    language_hash.each do |language_name, type|
+      if new_hash.key?(language_name)
+      new_hash[language_name][:style] << style_key
+
+    else
+      new_hash[language_name] = type
+      new_hash[language_name][:style] = [style_key]
+
 end
+end
+end
+  new_hash
+end
+
+#add :style => [] within each language, with :oo and/or :functional
