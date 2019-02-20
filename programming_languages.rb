1bd3828ce45e367ada01dad434264535d2a01d42
Matthew Shin <mshin1995@gmail.com>
2019-02-20 04:00:00 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, hash|
+    hash.each do |name, type|
+      new_hash[name] ||= type
+      new_hash[name][:style] ||= []
+      new_hash[name][:style] << style
+    end
+  end
+  new_hash
 end
